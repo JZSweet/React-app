2510b9697b6353ec7580e43b61245986e889b207
JZSweet <sweet_zhaozhao@live.cn>
2021-05-02 11:30:39 -0700

    add img

--- src/components/Thing.js
+++ src/components/Thing.js
@@ -1,8 +1,20 @@
 import React, { Component } from "react";
+import thing from "../Img/Appa.png";
+import { Container } from "react-bootstrap";
 
 class Thing extends Component {
-	render() {
-	  return <div style={{marginLeft: this.props.left + 'px', marginTop: this.props.top + 'px'}}>Thing</div>;
+  render() {
+    return (
+     <Container>
+    <div style={{ marginLeft: this.props.left + 'px', marginTop: this.props.top + 'px' }}>
+      <img
+        className="d-block w-100"
+        src={thing}
+        alt="move"
+      />
+    </div>
+    </Container>
+    )
   }
 }
 
